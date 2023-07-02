5e458698f7509bfc9f0e3c95e22688e203316403
Feras Jobeir <fjobeir@gmail.com>
2023-07-02 19:12:13 +0300

    Initial Commit

---
+++ bbquote/bbquoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  bbquoteApp.swift
+//  bbquote
+//
+//  Created by Feras Jobeir on 2.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct bbquoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
